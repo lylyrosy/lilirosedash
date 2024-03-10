78f738dadb28805477880a299369aae3f496d699
lylyrosy <lylyrose51@gmail.com>
2024-03-10 19:21:50 +0100

    hhhhhhhh

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+    base: '/lilirosedash/',
+
+    build: {
+      outDir: 'build' // Définir le dossier de sortie sur 'build'
+    }
+ 
 })
